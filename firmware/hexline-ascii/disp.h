b362f688666e28dcea7eb33b04b911273fd7d482
Simon de Vlieger <supakeen@redhat.com>
2024-12-26 09:36:58 +0100

    disp: move `area` into `disp`
    To start creating a separate driver header and implementation let's
    start with moving everything relevant into `disp`; which will provide
    different drivers to speak to different screens with a normal interface.
    
    In the first commit this is just moving some things around before I
    focus on improving the display driver itself.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- firmware/hexline-ascii/disp.h
+++ firmware/hexline-ascii/disp.h
@@ -6,7 +6,24 @@
 #define DISP_PIN 4
 #define DISP_BAUD 1500000
 
-void disp_init(void);
-void disp_draw(uint16_t*);
+#define AREA_CHAR_C 32  // there are 32 characters per area
 
+#define AREA_SEG_C 16   // and each character has 16 segments
+#define AREA_SEG_W 2    // and each segment has a width of 2 bytes (16-bits)
+
+// giving us a total width of CHAR_C * SEG_C * SEG_W plus
+// the end of frame marker (4)
+#define AREA_SIZE AREA_CHAR_C * AREA_SEG_C
+
+struct disp {
+    uint16_t area[AREA_SIZE];
+};
+
+struct disp* disp_init();
+
+void disp_draw(struct disp*);
+void disp_clear(struct disp*);
+
+void disp_text_set(struct disp*, char*);
+void disp_text_set_at_position(struct disp*, int, char);
 #endif
