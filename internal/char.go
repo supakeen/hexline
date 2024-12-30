25809f6e258736185a6030b3b5684a8e9d9c7444
Simon de Vlieger <supakeen@redhat.com>
2024-12-30 11:40:13 +0100

    display: cutoff strats
    Allows for dealing with lines-that-are-too-long dynamically by either
    cutting them off, or scrolling them.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- internal/char.go
+++ internal/char.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
+type CutoffStrategy int
+
+const (
+	CutoffStrategyINVALID CutoffStrategy = iota
+	CutoffStrategyCUT
+	CutoffStrategySCROLL
+)
+
 type CharDisplay struct {
 	file *os.File
 }
@@ -24,14 +32,44 @@ func NewCharDisplay(path string) (*CharDisplay, error) {
 	return &c, nil
 }
 
-func (c *CharDisplay) Display(text []string, delay int, keep bool) {
+func (c *CharDisplay) Display(text []string, delay int, strat CutoffStrategy, keep bool) {
 	for _, line := range text {
-		c.file.Write([]byte(fmt.Sprintf("\r%s", line)))
+		switch strat {
+		case CutoffStrategyCUT:
+			c.DisplayWithCutoff(line, 32)
+			break
+		case CutoffStrategySCROLL:
+			c.DisplayWithScroll(line, 32)
+			break
+		}
+
 		time.Sleep(time.Duration(delay) * time.Millisecond)
 	}
 
 	// If we're not keeping the text write a clear sequence at the end
 	if !keep {
 		c.file.Write([]byte("\r"))
+	}
+}
+
+func (c *CharDisplay) DisplayWithCutoff(text string, length int) {
+	if len(text) > length {
+		text = text[:length]
+	}
+
+	c.file.Write([]byte(fmt.Sprintf("\r%s", text)))
+}
+
+func (c *CharDisplay) DisplayWithScroll(text string, length int) {
+	if len(text) < length {
+		c.file.Write([]byte(fmt.Sprintf("\r%s", text)))
+	} else {
+		var temp string
+
+		for i := 0; i < (len(text) - length); i++ {
+			temp = text[i : i+length]
+			c.file.Write([]byte(fmt.Sprintf("\r%s", temp)))
+			time.Sleep(time.Duration(100) * time.Millisecond)
+		}
 	}
 }
