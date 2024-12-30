1518a899053c8a7918be111a9a49bac114ff9b67
Simon de Vlieger <supakeen@redhat.com>
2024-12-30 11:00:05 +0100

    cmd: get device file through internal
    This auto-ioctls.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ internal/device.go
@@ -0,0 +1,19 @@
+package internal
+
+import (
+    "os"
+)
+
+func GetDeviceFile(path string) (*os.File, error) {
+	f, err := os.OpenFile(path, os.O_RDWR, 0)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := SetRate(f, 115200); err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
